7a67000ba1c1b3520861ced56f311be2107ee7ff
Italo Bejarano <ibejarano.tercero@serviciosdeterceros.com.pe>
2025-06-06 19:43:16 -0500

    feat: enhance merged handler with DynamoDB integration

--- src/handlers/merged.ts
+++ src/handlers/merged.ts
@@ -1,21 +1,48 @@
 import { getRandomStarWarsFilm } from "../services/swapiService";
 import { getWikipediaSummary } from "../services/mediaWikiService";
 
+import * as uuid from "uuid";
+import { DynamoDB } from "aws-sdk";
+const dynamoDb = new DynamoDB.DocumentClient();
+
 export const merged = async () => {
-  const film = await getRandomStarWarsFilm();
-  const summary = await getWikipediaSummary(film?.title);
+  try {
+    const film = await getRandomStarWarsFilm();
+    const summary = await getWikipediaSummary(film?.title);
 
-  const response = {
-    title: film?.title || "Unknown Title",
-    opening_crawl: film?.opening_crawl || "No opening crawl available.",
-    director: film?.director || "Unknown Director",
-    producer: film?.producer || "Unknown Producer",
-    release_date: film?.release_date || "Unknown Release Date",
-    summary: summary,
+    const response = {
+      title: film?.title || "Unknown Title",
+      opening_crawl: film?.opening_crawl || "No opening crawl available.",
+      director: film?.director || "Unknown Director",
+      producer: film?.producer || "Unknown Producer",
+      release_date: film?.release_date || "Unknown Release Date",
+      summary: summary,
+    };
+
+    const timestamp = new Date().getTime();
+
+    const params = {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        id: uuid.v1(),
+        response,
+        createdAt: timestamp,
+        updatedAt: timestamp,
+      },
+    };
+
+    await dynamoDb.put(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+    
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Error in merged handler" }),
+    };
   }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response),
-  };
 };
